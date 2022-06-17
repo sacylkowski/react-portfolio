245431d3db381fe4d162d85c56ea47756c7c1e21
sacylkowski <sacylkowski@gmail.com>
2022-06-16 20:17:39 -0500

    added more information in about me

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -6,6 +6,10 @@ function About() {
         <section>
             <h1>Sarah Cylkowski</h1>
             <img src={myself} style={{ width: "30% " }} alt="portrait" />
+            <p>I was born and raised in Illinois before moving to Austin, TX in 2014. I graduated from Illinois State
+            Universty in 2013 with a studio art degree with a focus in Video and a minor in Business Adminastruation. I
+            have a background in sales, human resources, and marketing and have been working in those areas for 7+ years.  
+            I am very excited to be on a new carrer path and to continue to learn and grow in this field.  Check out my projects and contact me for more information.</p>
         </section>
     )
 }
