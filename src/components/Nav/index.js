fbd46b0edcb85eabfa1995520569340a65b38259
sacylkowski <sacylkowski@gmail.com>
2022-06-18 12:40:58 -0500

    added Projects component

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+// import Projects from "..";
 
 function Nav() {
     return (
---
+++ src/components/Projects/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+// const categories = [
+//     {
+//       name: "commercial",
+//       description:
+//         "Photos of grocery stores, food trucks, and other commercial projects",
+//     },
+//     { name: "portraits", description: "Portraits of people in my life" },
+//     { name: "food", description: "Delicious delicacies" },
+//     { name: "landscape", description: "Fields, farmhouses, waterfalls, and the beauty of nature",
+//     },
+//   ];
+
+function Projects() {
+
+    return (
+        <section>
+        <h1>Title</h1>
+        <p>Description</p> 
+      </section>
+    )
+  }
+
+  export default Projects;
