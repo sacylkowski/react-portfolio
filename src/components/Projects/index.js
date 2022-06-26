90bea1bdc61f1a475da0e5a1ce9a3593656a4a5b
sacylkowski <sacylkowski@gmail.com>
2022-06-26 16:03:21 -0500

    projects added

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import dandd from "../../assets/img/WizardoftheCode.png";
+import quiz from "../../assets/img/Coding-quiz.png";
+import generator from "../../assets/img/PW-generator.png";
 
 
 function Projects() {
@@ -10,6 +12,16 @@ function Projects() {
         <h1>D & D character generator</h1>
         <p>Description</p> 
         <img src={dandd} style={{ width: "50% " }} alt="D&D character generator" />
+        </div>
+        <div className="project2">
+        <h1>Password generator</h1>
+        <p>Description</p> 
+        <img src={generator} style={{ width: "50% " }} alt="PW generator" />
+        </div>
+        <div className="project3">
+        <h1>Coding Quiz</h1>
+        <p>Description</p> 
+        <img src={quiz} style={{ width: "50% " }} alt="Coding Quiz" />
         </div>
       </section>
     )
