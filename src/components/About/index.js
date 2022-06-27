583694ce2d088a0cfdb41ecdb993cc3bf41d159f
sacylkowski <sacylkowski@gmail.com>
2022-06-27 10:46:36 -0500

    portfolio complete

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -8,8 +8,7 @@ function About() {
             <div className="about-me">
                 <h2>Sarah Cylkowski</h2>
                 <p>I was born and raised in Illinois before moving to Austin, TX in 2014. I graduated from Illinois State
-                    Universty in 2013 with a studio art degree with a focus in Video and a minor in Business Adminastruation. I
-                    have a background in sales, human resources, and marketing and have been working in those areas for 7+ years.
+                    Universty, majoring in studio arts with a focus in Video and a minoring in Business Adminastruation.  I love Harry Potter and enjoy baking many different things.
                     I am very excited to be on a new carrer path and to continue to learn and grow in this field.  Check out my projects and contact me for more information.</p>
             </div>
         </section>
--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -15,41 +15,41 @@ function Projects() {
         <div className="column">
           <h1>D & D character generator</h1>
           <p>This app lets you create D & D characters and save them to your account when you are logged in. <br />
-          <a href="https://github.com/sacylkowski/wizards-code">GitHub Repo</a> <a href="https://warm-cove-70741.herokuapp.com/">Live Site</a></p>
+            <a href="https://github.com/sacylkowski/wizards-code">GitHub Repo</a> <a href="https://warm-cove-70741.herokuapp.com/">Live Site</a></p>
           <img src={dandd} style={{ width: "80% " }} alt="D&D character generator" />
         </div>
         <div className="column">
           <h1>Password generator</h1>
           <p>This app asks the user a series of questions regarding what they would like in a password.  The new password is then generated.<br />
-          <a href="https://github.com/sacylkowski/password-generator">GitHub Repo</a> <a href="https://sacylkowski.github.io/password-generator/">Live Site</a></p>
+            <a href="https://github.com/sacylkowski/password-generator">GitHub Repo</a> <a href="https://sacylkowski.github.io/password-generator/">Live Site</a></p>
           <img src={generator} style={{ width: "80% " }} alt="PW generator" />
         </div>
       </div>
       <div className="row">
         <div className="column">
-        <h1>Employee Tracker</h1>
+          <h1>Employee Tracker</h1>
           <p>This is a commandline app that where you add information about each team member to create a directory.  The database is made into an HTML file that you can view afterwards.<br />
-          <a href="https://github.com/sacylkowski/employee-tracker">GitHub Repo</a></p>
+            <a href="https://github.com/sacylkowski/employee-tracker">GitHub Repo</a></p>
           <img src={tracker} style={{ width: "80% " }} alt="Employee Tracker" />
         </div>
         <div className="column">
           <h1>Budget Tracker</h1>
           <p>This is a PWA that keeps track of your expenses.  You can add and subtract funds online and offline. <br />
-          <a href="https://github.com/sacylkowski/budget-tracker">GitHub Repo</a> <a href="https://pure-basin-22816.herokuapp.com/">Live Site</a></p>
+            <a href="https://github.com/sacylkowski/budget-tracker">GitHub Repo</a> <a href="https://pure-basin-22816.herokuapp.com/">Live Site</a></p>
           <img src={budget} style={{ width: "80% " }} alt="Budget tracker" />
         </div>
       </div>
       <div className="row">
         <div className="column">
-        <h1>README generator</h1>
+          <h1>README generator</h1>
           <p>This commandline run program asks the user questions about their project and generates a professional README.<br />
-          <a href="https://github.com/sacylkowski/README-generator">GitHub Repo</a></p>
+            <a href="https://github.com/sacylkowski/README-generator">GitHub Repo</a></p>
           <img src={readme} style={{ width: "80% " }} alt="Readme generator" />
         </div>
         <div className="column">
           <h1>Vacation Vibes</h1>
           <p>This app was created to help travelers find places to eat in a new city.  It uses the Yelp API to retrieve the information.<br />
-          <a href="https://github.com/sacylkowski/Vacation-vibes">GitHub Repo</a> <a href="https://peterwmcclelland.github.io/Vacation-vibes/">Live Site</a></p>
+            <a href="https://github.com/sacylkowski/Vacation-vibes">GitHub Repo</a> <a href="https://peterwmcclelland.github.io/Vacation-vibes/">Live Site</a></p>
           <img src={vacation} style={{ width: "80% " }} alt="Vacation Vibes" />
         </div>
       </div>
