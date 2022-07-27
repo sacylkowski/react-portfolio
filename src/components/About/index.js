d149e34925664195a96c2c77192dc6fd7cfe743c
sacylkowski <sacylkowski@gmail.com>
2022-07-27 10:10:39 -0500

    updated bio

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -8,8 +8,8 @@ function About() {
             <div className="about-me">
                 <h2>Sarah Cylkowski</h2>
                 <p>I was born and raised in Illinois before moving to Austin, TX in 2014. I graduated from Illinois State
-                    Universty, majoring in studio arts with a focus in Video and a minoring in Business Adminastruation.  I love Harry Potter and enjoy baking many different things.
-                    I am very excited to be on a new carrer path and to continue to learn and grow in this field.  Check out my projects and contact me for more information.</p>
+                    Universty, majoring in studio arts with a focus in Video and a minoring in Business Administration.  I love Harry Potter and enjoy baking new creations.
+                    I am looking forward to my new career path and to continue to learn and grow in this field.  Check out my projects and contact me for more information.</p>
             </div>
         </section>
     )
