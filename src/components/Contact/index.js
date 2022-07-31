24def7ade5f38406bab2706076572db7909ea530
sacylkowski <sacylkowski@gmail.com>
2022-07-31 15:49:38 -0500

    added contact functionality

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,14 +1,14 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from 'react';
 import validateEmail from "../../utils/helpers";
 import { MdOutlineMail } from "react-icons/md";
 import { BiMessageDetail } from "react-icons/bi";
+import emailjs from '@emailjs/browser';
 
 function Contact() {
-
+    // creating state variables for form fields
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [message, setMessage] = useState("");
-
 
     const [errorMessage, setErrorMessage] = useState("");
 
@@ -27,15 +27,18 @@ function Contact() {
         }
     };
 
-    const handleFormSubmit = (e) => {
-        // Preventing the default behavior of the form submit (which is to refresh the page)
+    const form = useRef();
+
+    const sendEmail = (e) => {
         e.preventDefault();
 
-        // First we check to see if the email is not valid or if the userName is empty. If so we set an error message to be displayed on the page.
-        if (!validateEmail(email) || !name) {
-            setErrorMessage('Email or name is invalid');
-            return;
-        }
+        emailjs.sendForm('service_7mowx4c', 'template_ps75pgk', form.current, 'xHGXUZw2o2tNSuXtL')
+            .then((result) => {
+                console.log(result.text);
+                alert("Your message was successfully submitted!");
+            }, (error) => {
+                console.log(error.text);
+            });
 
         // clear the input after
         setName("");
@@ -43,13 +46,28 @@ function Contact() {
         setMessage("");
     };
 
+    // const handleFormSubmit = (e) => {
+    //     // Preventing the default behavior of the form submit (which is to refresh the page)
+    //     e.preventDefault();
+
+    //     // First we check to see if the email is not valid or if the userName is empty. If so we set an error message to be displayed on the page.
+    //     if (!validateEmail(email) || !name) {
+    //         setErrorMessage('Email or name is invalid');
+    //         return;
+    //     } else {
+    //         console.log(name, email, message);
+    //         alert("Your message was successfully submitted!");
+    //     }
+
+    //     // clear the input after
+    //     setName("");
+    //     setEmail("");
+    //     setMessage("");
+    // };
+
     return (
         <section id="contact-info" className="contactMe">
-            {/* <div className="contact">
-                <p className="section-title">Get In Touch!< br />
-                    Fill out the form, or feel free to e-mail me at:  <a href="mailto:sacylkowski@gmail.com" className="myEmail">Sacylkowski@gmail.com</a></p>
-            </div> */}
-            <form action="" className="contact-form">
+            <form ref={form} className="contact-form">
                 <h2>Get in touch:</h2>
                 <div>
                     <label htmlFor="name">Name:    </label>
@@ -62,7 +80,7 @@ function Contact() {
                     <label htmlFor="message">Message: </label>
                     <textarea name="message" rows="7" value={message} onChange={handleInputChange} />
                 </div>
-                <button type="submit" className="messageBtn" onClick={handleFormSubmit} onChange={handleInputChange}>Send message</button>
+                <button type="submit" className="messageBtn" onClick={sendEmail}>Send message</button>
             </form>
             {errorMessage && (
                 <div>
@@ -71,12 +89,12 @@ function Contact() {
             )}
             <div className="contacts">
                 <article className="contactEmail">
-                <a href="mailto:sacylkowski@gmail.com"><MdOutlineMail size={70}/></a>
+                    <a href="mailto:sacylkowski@gmail.com"><MdOutlineMail size={70} /></a>
                     <p className="emailText">Send me an e-mail<br />Sacylkowski@gmail.com</p>
                 </article>
                 <article className="contactLinkedin">
-                <a href="https://www.linkedin.com/in/sarahcylkowski/" target="_blank" rel="noreferrer"><BiMessageDetail size={70}/></a>
-                <p className="">Send me a message on<br />LinkedIn</p>
+                    <a href="https://www.linkedin.com/in/sarahcylkowski/" target="_blank" rel="noreferrer"><BiMessageDetail size={70} /></a>
+                    <p className="">Send me a message on<br />LinkedIn</p>
                 </article>
             </div>
         </section>
